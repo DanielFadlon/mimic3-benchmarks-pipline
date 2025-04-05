fa66e12ffaffb5aff9c5cdff6fb0012edb6fa3de
Daniel Fadlon <dfadlon@salesforce.com>
2025-04-05 22:11:54 +0300

    iw - processor

---
+++ src/period_file_processor/iw_period_processor/iw_period_processor.py
@@ -0,0 +1,88 @@
+from typing import Dict, Optional, Callable
+
+import numpy as np
+import pandas as pd
+from src.file_utils import load_json
+from src.period_file_processor.string_values_handler import number_to_gsw_str
+from src.period_file_processor.period_file_processor import PeriodFileProcessor
+
+HOURS_TEMP_COLUMN_NAME = 'hours_tmp'
+
+class IWPeriodProcessor(PeriodFileProcessor):
+    def __init__(self,
+                 task: str,
+                 set_type: str,
+                 history_volume: int,
+                 agg_func: Optional[Callable] = max, # max, mean, median
+                 interval_size: Optional[str] = None,
+                 feature_name_mapping_path: Optional[str] = "configs/feature_name_mapping.json"):
+        super().__init__(task, set_type)
+        self.history_volume = history_volume
+        self.interval_size = interval_size
+        self.agg_func = agg_func
+        self.feature_name_mapping = load_json(feature_name_mapping_path)
+
+
+    def process_instance(self, instance):
+        super().process_instance(instance)
+        df = super().episode_numpy_to_dataframe(instance)
+
+        df[HOURS_TEMP_COLUMN_NAME] = df['Hours'].astype(int)
+        if self.interval_size:
+            df[HOURS_TEMP_COLUMN_NAME] = df['Hours'].transform(lambda x: int((x // self.interval_size) * self.interval_size))
+            # GSW to numeric
+            for col in self.string_types_columns:
+                df[col] = df[col].apply(lambda s: int(s[0]) if s is not None else s)
+            df = df.groupby([HOURS_TEMP_COLUMN_NAME]).agg(self.agg_func).reset_index()
+            # Back to GSW str value
+            for col in self.string_types_columns:
+                df[col] = df[col].apply(lambda n: number_to_gsw_str(col, n))
+
+        df = df.sort_values(by=['Hours'])
+        history_text = ""
+        for feat in df.columns:
+            if feat in [HOURS_TEMP_COLUMN_NAME, 'Hours', 'episode']:
+                continue
+
+            history_data = self._get_history_for_feature(df, feat)[-self.history_volume:]
+            history_text += self.feature_name_mapping.get(feat, feat)
+            history_text += ': '
+            for hist_obj in history_data:
+                val = hist_obj['value']
+                time = hist_obj['time']
+                history_text += f'{val} (t={time}), '
+            history_text = history_text[:-2] + '.\n' if len(history_data) > 0 else history_text + "None.\n"
+
+
+        current_time_passed_in_hours = df[HOURS_TEMP_COLUMN_NAME].iloc[-1]
+        prefix = f"The patient's hospital journey began at time t=0 hours and has now reached t={current_time_passed_in_hours} hours."
+        text = f'{prefix}\n\n{history_text}\n'
+
+        res_instance = pd.Series({'episode': df['episode'].iloc[0], 'text': text, 'Hours': df['Hours'].iloc[-1]})
+
+        # Extract to function
+        if self.task in ["in-hospital-mortality", "decompensation"]:
+            res_instance['label'] = instance['y']
+        elif self.task in ['phenotyping', 'multitask']:
+            pass # TODO
+
+        return res_instance
+
+    @staticmethod
+    def _get_history_for_feature(sorted_df: pd.DataFrame, feature_col_name: str):
+        previous_value = -1
+        history = []
+
+        for i, instance_i in sorted_df.iterrows():
+            # Floor to interval hours if needed
+            time_i = int(instance_i[HOURS_TEMP_COLUMN_NAME])
+            # First instance - init the history list
+            if previous_value == -1:
+                history.append({"time": time_i, "value": instance_i[feature_col_name]})
+            # If the value has changed, add the new value to the history list
+            elif (not pd.isna(instance_i[feature_col_name])) and instance_i[feature_col_name] != previous_value:
+                history.append({"time": time_i, "value": instance_i[feature_col_name]})
+
+            previous_value = instance_i[feature_col_name]
+
+        return history
---
+++ src/period_file_processor/string_values_handler.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+GSW_STR_COLUMNS_NAMES = ['Glascow_coma_scale_eye_opening', 'Glascow_coma_scale_motor_response', 'Glascow_coma_scale_verbal_response']
+
+def unique_handle_for_non_numeric_gsw(df: pd.DataFrame):
+    if 'Glascow_coma_scale_eye_opening' in df.columns:
+        value_mapping = {
+            'Spontaneously': '4 Spontaneously',
+            'To Speech': '3 To Speech',
+            'To Pain': '2 To Pain',
+            'No response': '1 No response',
+        }
+        df['Glascow_coma_scale_eye_opening'].replace(value_mapping, inplace=True)
+        non_digit_count = df['Glascow_coma_scale_eye_opening'].str.match(r'^[^\d]').sum()
+        if non_digit_count > 0:
+            print("WARNING - Glascow_coma_scale_eye_opening --- non digit start value counts -- ", non_digit_count)
+            print(df['Glascow_coma_scale_eye_opening'][df['Glascow_coma_scale_eye_opening'].str.match(r'^[^\d]')])
+    if 'Glascow_coma_scale_motor_response' in df.columns:
+        value_mapping = {
+            'Obeys Commands': '6 Obeys Commands',
+            'Localizes Pain': '5 Localizes Pain',
+            'Flex-withdraws': '4 Flex-withdraws',
+            'Abnormal Flexion': '3 Abnormal Flexion',
+            'Abnormal extension': '2 Abnormal extension',
+            'No response': '1 No response'
+        }
+        df['Glascow_coma_scale_motor_response'].replace(value_mapping, inplace=True)
+        non_digit_count = df['Glascow_coma_scale_motor_response'].str.match(r'^[^\d]').sum()
+        if non_digit_count > 0:
+            print("WARNING - Glascow_coma_scale_motor_response --- non digit start value counts -- ", non_digit_count)
+            print(df['Glascow_coma_scale_motor_response'][df['Glascow_coma_scale_motor_response'].str.match(r'^[^\d]')])
+
+    if 'Glascow_coma_scale_verbal_response' in df.columns:
+        value_mapping = {
+            'Oriented': '5 Oriented',
+            'Confused': '4 Confused',
+            'Inappropriate Words': '3 Inappropriate Words',
+            'Incomprehensible sounds': '2 Incomprehensible sounds',
+            'No response': '1 No response',
+            'No Response': '1 No Response',
+            'No Response-ETT': '1 No Response-ETT'
+        }
+        df['Glascow_coma_scale_verbal_response'].replace(value_mapping, inplace=True)
+        non_digit_count = df['Glascow_coma_scale_verbal_response'].str.match(r'^[^\d]').sum()
+        if non_digit_count > 0:
+            print("WARNING - Glascow_coma_scale_verbal_response --- non digit start value counts -- ", non_digit_count)
+            print(df['Glascow_coma_scale_verbal_response'][df['Glascow_coma_scale_verbal_response'].str.match(r'^[^\d]')])
+    return df
+
+
+def number_to_gsw_str(col_name, number):
+    if pd.isna(number):
+        return number
+
+    number = int(number)
+    map_n_to_gsw = {
+        'Glascow_coma_scale_eye_opening': {
+            4: 'Spontaneously',
+            3: 'To Speech',
+            2: 'To Pain',
+            1: 'No response'
+        },
+        'Glascow_coma_scale_motor_response': {
+            6: 'Obeys Commands',
+            5: 'Localizes Pain',
+            4: 'Flex-withdraws',
+            3: 'Abnormal Flexion',
+            2: 'Abnormal extension',
+            1: 'No response'
+        },
+        'Glascow_coma_scale_verbal_response': {
+            5: 'Oriented',
+            4: 'Confused',
+            3: 'Inappropriate Words',
+            2: 'Incomprehensible sounds',
+            1: 'No response',
+        },
+    }
+    return map_n_to_gsw[col_name][number]
